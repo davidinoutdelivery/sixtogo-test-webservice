1f91cdbf3375f296bc7660c3e17022f57129b74a
ddominguezinoutdelivery <david.dominguez@inoutdelivery.com>
2018-08-14 16:48:44 -0500

    V0.00.000 - 14/08/2018 16:45 (DADR) Versión inicial: 1.	Aplicación base Yii2. 2.	PhpOrient  instalado. 3.	Parámetros de base de datos orintdb configurados. 4.	Parámetros de temas configurados. 5.	Creada la carpeta de themes para almacenar los temas. 6.	Creado el tema sixtogo. 7.	Configurado el urlManager de Yii2.

---
+++ config/params.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'orientDb'=> [
+        'hostname' => 'orientdb.mx.inoutdelivery.com',
+        'port' => 2424,
+        'dbname' => 'brain',
+        'username' => 'admin',
+        'password' => 'inout2018%$',
+    ],
+];
---
+++ config/web.php
@@ -0,0 +1,81 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request' => [
+            'cookieValidationKey' => '-BgLebI3QZV9r9DS6krJNmi5-NQgyHKr',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => true,
+            'rules' => [
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                '' => '/site/index',
+            ],
+        ],
+        'view' => [
+            'theme' => [
+                'pathMap' => ['@app/views' => '@app/themes/sixtogo'],
+                'baseUrl' => '@web/themes/sixtogo',
+            ],
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
